b89f284816d9e9de2f471a25857dd3cd73018135
Arshi Khwaja <arshikhwaja@gmail.com>
2023-07-25 16:12:30 +0530

    First Commit

---
+++ app/src/main/java/com/firstapp/MainActivity.java
@@ -0,0 +1,137 @@
+package com.firstapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    TextView welcomeTextView;
+    EditText messageEditText;
+    ImageButton sendButton;
+    List<Message> messageList;
+    MessageAdapter messageAdapter;
+    public static final MediaType JSON
+            = MediaType.get("application/json; charset=utf-8");
+    OkHttpClient client = new OkHttpClient. Builder()
+            .readTimeout(20, TimeUnit.SECONDS)
+            .build();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        messageList = new ArrayList<>();
+
+        recyclerView = findViewById(R.id.recycler);
+        welcomeTextView = findViewById(R.id.welcome_text);
+        messageEditText = findViewById(R.id.edit_message);
+        sendButton = findViewById(R.id.send_button);
+
+        //setup recycler view
+        messageAdapter = new MessageAdapter(messageList);
+        recyclerView.setAdapter(messageAdapter);
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        llm.setStackFromEnd(true); //for scrolling below to up
+        recyclerView.setLayoutManager(llm);
+
+        sendButton.setOnClickListener((v)->{
+            String question = messageEditText.getText().toString().trim();
+            addToChat(question, Message.SENT_BY_ME);
+            messageEditText.setText("");
+            callAPI(question);
+            welcomeTextView.setVisibility(View.GONE);
+        });
+    }
+
+    // sent msg added to message list
+    void addToChat(String message, String sentBy){
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                messageList.add(new Message(message, sentBy));
+                messageAdapter.notifyDataSetChanged();
+                recyclerView.smoothScrollToPosition(messageAdapter.getItemCount()); //bring to last position
+            }
+        });
+    }
+
+    void addResponse(String response){
+        messageList.remove(messageList.size()-1);
+        addToChat(response, Message.SENT_BY_BOT);
+    }
+    //okhttp setup
+    void callAPI(String question){
+        messageList.add(new Message("Typing...", Message.SENT_BY_BOT));
+        JSONObject jsonBody = new JSONObject();
+        try {
+            jsonBody.put("model","gpt-3.5-turbo");
+            JSONArray msgArray = new JSONArray();
+            JSONObject obj = new JSONObject();
+            obj.put("role","user");
+            obj.put("content",question);
+            msgArray.put(obj);
+            jsonBody.put("messages",msgArray);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        RequestBody body = RequestBody.create(jsonBody.toString(),JSON);
+        Request request = new Request.Builder()
+                .url("https://api.openai.com/v1/chat/completions")
+                .header("Authorization", "Bearer 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")
+                .post(body)
+                .build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                addResponse("Failed to load response due to "+ e.getMessage());
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                if(response.isSuccessful()){
+                    JSONObject jsonObject=null;
+                    try {
+                        jsonObject = new JSONObject(response.body().string());
+                        JSONArray jsonArray = jsonObject.getJSONArray("choices");
+                        String result = jsonArray.getJSONObject(0).getJSONObject("message").getString("content");
+                        addResponse(result.trim());
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+                else {
+                    addResponse("Failed to load response due to "+ response.body().string());
+                }
+            }
+        });
+    }
+}
